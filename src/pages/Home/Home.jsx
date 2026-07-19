3ddb760e2022e90d3695d18f9dc266eea6c903b8
Gurleen <gurleenmaan2006@gmail.com>
2026-07-19 23:40:01 +0530

    Added hero section

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,12 @@
 import Navbar from "../../components/Navbar/Navbar";
+import Hero from "../../components/Hero/Hero";
+import heroImage from "../../assets/images/hero-image.png";
 
 function Home() {
   return (
     <>
       <Navbar />
+      <Hero/>
 
       <div>
         
