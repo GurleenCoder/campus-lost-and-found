0a953d9d42d9713e3c8b7e0ff2efaece8d9a4812
Gurleen <gurleenmaan2006@gmail.com>
2026-07-20 13:58:54 +0530

    Features section added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import Navbar from "../../components/Navbar/Navbar";
 import Hero from "../../components/Hero/Hero";
 import heroImage from "../../assets/images/hero-image.png";
+import Features from "../../components/Features/Features";
 
 function Home() {
   return (
     <>
       <Navbar />
       <Hero/>
+      <Features/>
 
       <div>
         
